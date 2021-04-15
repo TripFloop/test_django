e97d490db5d53dd6216987762d63b1a19de9dc58
TripFloop <delala.ra@yandex.ru>
2021-04-15 12:04:55 +0300

    9 step: Added middleware adding <! - HelloWorld > string before closing header

---
+++ news_app/middlewares.py
@@ -0,0 +1,13 @@
+class EditHTMLMiddleware:
+    def __init__(self, get_response):
+        self._get_response = get_response
+
+    def __call__(self, request):
+        response = self._get_response(request)
+        index = response.content.find(b"</header>")
+        # response.content is bytestring, so to concatenate we need to decode it
+        content_decoded = response.content.decode()
+        new_content = content_decoded[:index] + "<! - HelloWorld >" + content_decoded[index:]
+        # after need to encode it back
+        response.content = new_content.encode("utf-8")
+        return response
--- test_project/settings.py
+++ test_project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "news_app.middlewares.EditHTMLMiddleware"
 ]
 
 ROOT_URLCONF = 'test_project.urls'
